d3c42aa3517abc6ae18ebbf3fc6c7ce6dc2725db
Mateusz Tyc <tycmat9@gmail.com>
2025-12-26 19:45:04 -0500

    feat: simplify fsm

--- src/ice_comms_service.c
+++ src/ice_comms_service.c
@@ -1,68 +1,63 @@
 
+#include "ice_crc.h"
 #include <stdint.h>
 
+typedef enum {
+    PACKET_CRC_ERROR   = -1,
+    PACKET_IN_PROGRESS = 0,
+    PACKET_READY       = 1,
+} ice_packet_status_t;
 typedef struct {
-
     uint8_t state;
-    uint8_t frame_buffer[256];
-    uint8_t payload_counter;
+    uint8_t frame_buffer[260];
+    uint8_t counter;
     uint8_t expected_len;
-
 } ice_comms_ctx_t;
 
-typedef enum {
-    STATE_IDLE = 0,
-    STATE_LEN,
-    STATE_PAYLOAD,
-    STATE_CRC
-} ice_comms_state_t;
+typedef enum { STATE_IDLE = 0, STATE_LEN, STATE_BODY } ice_comms_state_t;
 
 #define SOF (0xAAU)
 
-static void ice_reset_state(ice_comms_ctx_t* ctx){
-    ctx->state = STATE_IDLE;
-    ctx->payload_counter = 0;
+static void ice_reset_state(ice_comms_ctx_t *ctx) {
+    ctx->state        = STATE_IDLE;
+    ctx->counter      = 0;
     ctx->expected_len = 0;
 }
 
-void ice_process_byte(ice_comms_ctx_t* ctx, uint8_t byte) {
+ice_packet_status_t ice_process_byte(ice_comms_ctx_t *ctx, uint8_t byte) {
+    switch (ctx->state) {
+    case STATE_IDLE:
+        if (byte == SOF) {
+            ctx->state = STATE_LEN;
+        }
+        break;
+    case STATE_LEN:
+        ctx->expected_len = byte;
+        ctx->counter      = 0;
+        ctx->state        = STATE_BODY;
+        break;
+    case STATE_BODY:
+        ctx->frame_buffer[ctx->counter++] = byte;
 
-    switch(ctx->state){
-        case STATE_IDLE:
-            if (byte == SOF){
-                ctx->payload_counter = 0;
-                ctx->expected_len = 0;
-                ctx->state = STATE_LEN;
-            }
-            break;
-        case STATE_LEN:
-            ctx->expected_len = byte;
-            if (ctx->expected_len == 0){
-                ctx->state = STATE_CRC;
-            } else {
-                ctx->state = STATE_PAYLOAD;
-            }
-            break;
-        case STATE_PAYLOAD:
-            ctx->frame_buffer[ctx->payload_counter] = byte;
-            ctx->payload_counter++;
+        if (ctx->expected_len <= (ctx->counter + 2)) {
+            uint16_t received_crc = (uint16_t)ctx->frame_buffer[ctx->expected_len] |
+                                    ((uint16_t)ctx->frame_buffer[ctx->expected_len + 1] << 8);
+            uint16_t expected_crc = ice_crc_calcuate(ctx->frame_buffer, ctx->counter);
 
-            if (ctx->expected_len <= ctx->payload_counter){
-                ctx->state = STATE_CRC;
-            }
-            break;
-
-        case STATE_CRC:
-            if (byte == calc_crc(ctx->frame_buffer, ctx->payload_counter)){
-                done();
-            } else {
-                error();
-            }
             ice_reset_state(ctx);
-            break;
-        default:
-            ice_reset_state(ctx);
-            break;
+
+            if (received_crc == expected_crc) {
+                return PACKET_READY;
+            } else {
+                return PACKET_CRC_ERROR;
+            }
+        }
+        break;
+
+    default:
+        ice_reset_state(ctx);
+        break;
     }
 
+    return PACKET_IN_PROGRESS;
 }
