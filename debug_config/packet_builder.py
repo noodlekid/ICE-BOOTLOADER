6951ae6fe11a695c486f5418b275f46de7c712e6
Mateusz Tyc <tycmat9@gmail.com>
2025-12-27 11:54:49 -0500

    feat: working byte processing demo!

--- debug_config/packet_builder.py
+++ debug_config/packet_builder.py
@@ -1,8 +1,6 @@
 import struct
 from crccheck.crc import Crc16
 
-# 1. Define the Custom STM32 Hardware Configuration
-#    Poly: 0x755B (Koopman), Init: 0xFFFF, No Reflection
 class Crc16Koopman(Crc16):
     _width = 16
     _poly = 0x755B
@@ -12,13 +10,11 @@ class Crc16Koopman(Crc16):
     _xor_output = 0x0000
 
 def create_packet(payload_bytes):
-    # 2. Calculate CRC on the Payload ONLY
-    #    (The FSM calculates CRC on the buffer content, which is the payload)
     crc_inst = Crc16Koopman()
     crc_inst.process(payload_bytes)
     crc_val = crc_inst.final()
     
-    # 3. Build Frame: [SOF] [LEN] [PAYLOAD] [CRC_LSB] [CRC_MSB]
+    # Build Frame: [SOF] [LEN] [PAYLOAD] [CRC_LSB] [CRC_MSB]
     #    < = Little Endian
     #    B = Uint8 (SOF)
     #    B = Uint8 (LEN)
@@ -32,8 +28,7 @@ def create_packet(payload_bytes):
     
     return packet, crc_val
 
-# --- TEST ---
-payload = b'\xDD' # The command byte your C code expects
+payload = b'\xDD'
 packet, crc = create_packet(payload)
 
 print(f"Payload:  0x{payload.hex().upper()}")
--- src/app_main.c
+++ src/app_main.c
@@ -1,11 +1,38 @@
+#include "ice_comms_service.h"
 #include "ice_delay.h"
+#include "ice_transport_usb.h"
+#include "main.h"
+#include "stm32h7xx_ll_gpio.h"
 
-
+static uint8_t ok[]   = {'O', 'K'};
+static uint8_t nack[] = {'N', 'A', 'C', 'K'};
 
 int bootloader(void) {
     ICE_delay_init();
 
+    ICE_TRANSPORT_t usb_transport;
+    ice_comms_ctx_t comms_ctx;
+    ICE_TRANSPORT_usb_init(&usb_transport);
+    ice_comms_init(&comms_ctx);
+    uint8_t rx_byte;
+    ice_packet_status_t status;
+    uint32_t start = ICE_get_tick();
 
     while (1) {
+        if (usb_transport.ops->read(&usb_transport, &rx_byte, 1) > 0) {
+            status = ice_process_byte(&comms_ctx, rx_byte);
+            if (status == PACKET_READY) {
+                /* acknoweldge */
+                usb_transport.ops->write(&usb_transport, ok, sizeof(ok));
+            } else if (status == PACKET_CRC_ERROR) {
+                /* negative acknowledgement */
+                usb_transport.ops->write(&usb_transport, nack, sizeof(nack));
+            }
+        }
+
+        if (ICE_get_tick() - start > 500) {
+            LL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+            start = ICE_get_tick();
+        }
     }
 }
