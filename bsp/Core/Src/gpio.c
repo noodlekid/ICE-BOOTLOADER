cefcd4ec0e42e209dc4d244418db0dccd36a4036
Mateusz Tyc <tycmat9@gmail.com>
2025-12-27 09:58:02 -0500

    feat: fix analog debug pins

--- bsp/Core/Src/gpio.c
+++ bsp/Core/Src/gpio.c
@@ -90,8 +90,7 @@ void MX_GPIO_Init(void) {
     /**/
     GPIO_InitStruct.Pin = LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 |
                           LL_GPIO_PIN_4 | LL_GPIO_PIN_5 | LL_GPIO_PIN_6 | LL_GPIO_PIN_7 |
-                          LL_GPIO_PIN_8 | LL_GPIO_PIN_9 | LL_GPIO_PIN_10 | LL_GPIO_PIN_13 |
-                          LL_GPIO_PIN_14 | LL_GPIO_PIN_15;
+                          LL_GPIO_PIN_8 | LL_GPIO_PIN_9 | LL_GPIO_PIN_10 | LL_GPIO_PIN_15;
     GPIO_InitStruct.Mode = LL_GPIO_MODE_ANALOG;
     GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
     LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
--- src/app_main.c
+++ src/app_main.c
@@ -1,41 +1,11 @@
-#include "ice_comms_service.h"
 #include "ice_delay.h"
-#include "ice_transport.h"
-#include "ice_transport_usb.h"
-#include "main.h"
-#include "stm32h7xx_ll_gpio.h"
 
-static ICE_TRANSPORT_t trans;
 
-static const uint8_t ping_string[] = {'F', 'A', 'R', 'T', '\r', '\n'};
-static const uint8_t nack[]        = {'N', 'A', 'C', 'K'};
 
 int bootloader(void) {
     ICE_delay_init();
-    ICE_TRANSPORT_usb_init(&trans);
 
-    uint8_t rx_byte;
-    ice_comms_ctx_t usb_ctx;
 
-    uint32_t start = ICE_get_tick();
     while (1) {
-        if (ICE_get_tick() - start >= 500) {
-            LL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-            start = ICE_get_tick();
-        }
-
-        if (trans.ops->read(&trans, &rx_byte, 1) > 0) {
-            // If process_byte returns 1, we have a valid packet sitting in buffer
-            ice_packet_status_t status = ice_process_byte(&usb_ctx, rx_byte);
-            if (status == PACKET_READY) {
-                if (usb_ctx.frame_buffer[0] == 0xDD) {
-                    trans.ops->write(&trans, ping_string, sizeof(ping_string));
-                }
-
-                ice_comms_init(&usb_ctx);
-            } else if (status == PACKET_CRC_ERROR) {
-                trans.ops->write(&trans, nack, sizeof(nack));
-            }
-        }
     }
 }
